773d8431f38cfe903894569ce2e0ecb0caa26805
shohruh223 <shohruh.abd2223@gmail.com>
2023-11-22 12:10:55 +0500

    add sqlite

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
+import sqlite3
 
-"""Database haqida har bir branchda dars qo'yib boramiz"""
+
+connection = sqlite3.connect("d7.db")
+cursor = connection.cursor()
+
+
+cursor.execute("""
+    create table if not exists users(
+        id integer primary key ,
+        name varchar not null ,
+        age integer
+    )
+""")
+
+# create
+
+# cursor.execute("insert into users (name, age) values (?,?)", ("Asadbek", 14))
+# connection.commit()
+
+
+# read
+
+# users = cursor.execute("select id, name, age from users").fetchall()
+# for user in users:
+#     print(user)
+
+
+#  update
+
+# cursor.execute("update users set name=? where id=?", ("shohruh", 2))
+# connection.commit()
+
+
+#  delete
+
+cursor.execute("delete from users where id=?", (3,))
+connection.commit()
